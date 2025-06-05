67d1f99ed881c1a3ecf913bbc09105faaccc12a4
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2025-06-05 13:54:50 +0900

    Fix:update training script

--- src/skin_segmentor/train_segformer_ss.sh
+++ src/skin_segmentor/train_segformer_ss.sh
@@ -1,10 +1,11 @@
 # Run acne segmentation training
 accelerate launch src/skin_segmentor/train_segformer_ss.py \
     --model_name_or_path nvidia/mit-b5 \
-    --dataset_name thisiswooyeol/skin_acne \
+    --dataset_name ../dataset/acne \
     --do_reduce_labels \
-    --output_dir checkpoints/segformer-b5-acne-reduce-labels-focal+dice \
-    --num_train_epochs 100 \
+    --output_dir checkpoints/segformer-b5-focal+dice-acne50kdata-10ksteps \
+    --max_train_steps 10000 \
+    --checkpointing_steps epoch \
     --seed 42 \
     --with_tracking \
     --report_to wandb 
@@ -13,10 +14,11 @@ accelerate launch src/skin_segmentor/train_segformer_ss.py \
 # Run hemoglobin segmentation training
 accelerate launch src/skin_segmentor/train_segformer_ss.py \
     --model_name_or_path nvidia/mit-b5 \
-    --dataset_name thisiswooyeol/skin_hemo \
+    --dataset_name ../dataset/hemo \
     --do_reduce_labels \
-    --output_dir checkpoints/segformer-b5-hemo-reduce-labels-focal+dice \
-    --num_train_epochs 100 \
+    --output_dir checkpoints/segformer-b5-focal+dice-hemo6.6kdata-10ksteps \
+    --max_train_steps 10000 \
+    --checkpointing_steps epoch \
     --seed 42 \
     --with_tracking \
     --report_to wandb 
@@ -25,10 +27,11 @@ accelerate launch src/skin_segmentor/train_segformer_ss.py \
 # Run melanin segmentation training
 accelerate launch src/skin_segmentor/train_segformer_ss.py \
     --model_name_or_path nvidia/mit-b5 \
-    --dataset_name thisiswooyeol/skin_mela \
+    --dataset_name ../dataset/mela \
     --do_reduce_labels \
-    --output_dir checkpoints/segformer-b5-mela-reduce-labels-focal+dice \
-    --num_train_epochs 100 \
+    --output_dir checkpoints/segformer-b5-focal+dice-mela32kdata-10ksteps \
+    --max_train_steps 10000 \
+    --checkpointing_steps epoch \
     --seed 42 \
     --with_tracking \
     --report_to wandb
